d541756604dbbe3c9ea541b131b4c8610cfec976
Codekeeper2706 <kshitijsingh830320@gmail.com>
2025-08-15 14:37:53 +0530

    feat: add carousel component and integrate Navbar with authentication support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '../context/AuthProvider';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,4 +27,3 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     </html>
   );
 }
-
